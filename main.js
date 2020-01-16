678edf8d718bd5b31cb8f86e8acda6ccef6064b2
David Everett <davideverett1989@gmail.com>
2020-01-16 00:44:12 -0600

    Added a bit of JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Hello world.");
+document.getElementById("myButton").addEventListener("click", firstFunction);
+document.getElementById("secondButton").addEventListener("click", secondFunction);
+let arrayElements = document.getElementsByClassName("subflex");
+
+function firstFunction() {
+    document.getElementById("main").classList.toggle("reappear");
+  }
+
+function secondFunction() {
+  let randomNumber = Math.floor(Math.random() * 6);
+  arrayElements[randomNumber].classList.toggle("image");
+}
